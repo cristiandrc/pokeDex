c0d18391c6bab5b45ae67c1e600efd812687488b
Cristian Rojas <crisdarroca@hotmail.com>
2023-01-07 21:30:24 -0500

    consumir poke api

--- src/screens/PokeDex.jsx
+++ src/screens/PokeDex.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { getPokemonsApi } from "../api/pokemon";
 
 const PokeDex = () => {
+  const [pokemons, setPokemons] = useState();
+
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const { results } = await getPokemonsApi();
+      setPokemons(results);
+      console.log(pokemons);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <SafeAreaView>
       <Text>PokeDex</Text>
+      {pokemons?.map((e) => (
+        <Text>{e.name}</Text>
+      ))}
     </SafeAreaView>
   );
 };
