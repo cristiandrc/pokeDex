4b9afc6a03186281c88610ce9ce98c48769c7244
Cristian Rojas <crisdarroca@hotmail.com>
2023-01-08 23:29:03 -0500

    add header on pokemon details

--- src/navigation/PokedexNavigation.jsx
+++ src/navigation/PokedexNavigation.jsx
@@ -13,7 +13,11 @@ export default function PokeDexNavigation() {
         component={PokeDex}
         options={{ title: "", headerTransparent: true }}
       />
-      <Stack.Screen name="Pokemon" component={Pokemon} />
+      <Stack.Screen
+        name="Pokemon"
+        component={Pokemon}
+        options={{ title: "", headerTransparent: true }}
+      />
     </Stack.Navigator>
   );
 }
